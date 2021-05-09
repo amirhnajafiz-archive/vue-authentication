f23271535d0c28f8e2c3fa59298b16bf593deef3
9831065 <najafizadeh21@gmail.com>
2021-05-09 18:38:54 +0430

    router before each set

--- vue-auth/src/router/index.js
+++ vue-auth/src/router/index.js
@@ -50,3 +50,37 @@ let router = new Router({
       },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+      if (localStorage.getItem('jwt') == null) {
+          next({
+              path: '/login',
+              params: { nextUrl: to.fullPath }
+          })
+      } else {
+          let user = JSON.parse(localStorage.getItem('user'))
+          if(to.matched.some(record => record.meta.is_admin)) {
+              if(user.is_admin == 1){
+                  next()
+              }
+              else{
+                  next({ name: 'userboard'})
+              }
+          }else {
+              next()
+          }
+      }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if(localStorage.getItem('jwt') == null){
+          next()
+      }
+      else{
+          next({ name: 'userboard'})
+      }
+  }else {
+      next()
+  }
+})
+
+export default router
