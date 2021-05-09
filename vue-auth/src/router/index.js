393a8c0c2cffc8ac7f488924d28799a4404ac75b
9831065 <najafizadeh21@gmail.com>
2021-05-09 18:34:48 +0430

    router setup

--- vue-auth/src/router/index.js
+++ vue-auth/src/router/index.js
@@ -1,15 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import UserBoard from '@/components/UserBoard'
+import Admin from '@/components/Admin'
 
-Vue.use(Router)
-
-export default new Router({
+/* Defining out routers */
+let router = new Router({
+  mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      {
+          path: '/',
+          name: 'HelloWorld',
+          component: HelloWorld
+      },
+      {
+          path: '/login',
+          name: 'login',
+          component: Login,
+          meta: {
+              guest: true
+          }
+      },
+      {
+          path: '/register',
+          name: 'register',
+          component: Register,
+          meta: {
+              guest: true
+          }
+      },
+      {
+          path: '/dashboard',
+          name: 'userboard',
+          component: UserBoard,
+          meta: {
+              requiresAuth: true
+          }
+      },
+      {
+          path: '/admin',
+          name: 'admin',
+          component: Admin,
+          meta: {
+              requiresAuth: true,
+              is_admin : true
+          }
+      },
   ]
 })
