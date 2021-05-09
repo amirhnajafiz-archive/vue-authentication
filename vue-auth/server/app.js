5072a931dce88991bfa467ee05bf1c02ab66dbc1
9831065 <najafizadeh21@gmail.com>
2021-05-09 19:17:39 +0430

    plain server done with front

--- vue-auth/server/app.js
+++ vue-auth/server/app.js
@@ -54,13 +54,13 @@ router.post('/register', function (req, res) {
     db.insert([
         req.body.name,
         req.body.email,
-        hashSync(req.body.password, 8)
+        bcrypt.hashSync(req.body.password, 8)
     ],
         function (err) {
             if (err) return res.status(500).send("There was a problem registering the user.")
             db.selectByEmail(req.body.email, (err, user) => {
                 if (err) return res.status(500).send("There was a problem getting user")
-                let token = sign({ id: user.id }, secret, {
+                let token = jwt.sign({ id: user.id }, config.secret, {
                     expiresIn: 86400 // expires in 24 hours
                 });
                 res.status(200).send({ auth: true, token: token, user: user });
@@ -73,14 +73,14 @@ router.post('/register-admin', function (req, res) {
     db.insertAdmin([
         req.body.name,
         req.body.email,
-        hashSync(req.body.password, 8),
+        bcrypt.hashSync(req.body.password, 8),
         1
     ],
         function (err) {
             if (err) return res.status(500).send("There was a problem registering the user.")
             db.selectByEmail(req.body.email, (err, user) => {
                 if (err) return res.status(500).send("There was a problem getting user")
-                let token = sign({ id: user.id }, secret, {
+                let token = jwt.sign({ id: user.id }, config.secret, {
                     expiresIn: 86400 // expires in 24 hours
                 });
                 res.status(200).send({ auth: true, token: token, user: user });
@@ -88,14 +88,13 @@ router.post('/register-admin', function (req, res) {
         });
 });
 
-// User login router
 router.post('/login', (req, res) => {
     db.selectByEmail(req.body.email, (err, user) => {
         if (err) return res.status(500).send('Error on the server.');
         if (!user) return res.status(404).send('No user found.');
-        let passwordIsValid = compareSync(req.body.password, user.user_pass);
+        let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass);
         if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
-        let token = sign({ id: user.id }, secret, {
+        let token = jwt.sign({ id: user.id }, config.secret, {
             expiresIn: 86400 // expires in 24 hours
         });
         res.status(200).send({ auth: true, token: token, user: user });
@@ -108,6 +107,6 @@ app.use(router)
 // Now we start the server at port 3000
 let port = process.env.PORT || 3000;
 
-let server = app.listen(port, function() {
+let server = app.listen(port, function () {
     console.log('Express server listening on port ' + port)
 });
