f41169c46320f75450f3530f126dd0338c1efb4a
9831065 <najafizadeh21@gmail.com>
2021-05-11 11:30:01 +0430

    header nav created

--- vue-auth/src/App.vue
+++ vue-auth/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
+    <navbar />
     <img src="./assets/logo.png" />
     <router-view />
   </div>
 </template>
 
 <script>
+import navbar from './components/Nav'
+
 export default {
   name: "App",
+  components: {
+    navbar
+  }
 };
 </script>
 
 <style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ vue-auth/src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
